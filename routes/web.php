894f2be82ca9435126eb18354b1d4b421d88d6ef
Elisee-des <esabidani@gmail.com>
2023-07-01 00:27:39 +0000

    Debut Regsiter

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('register', 'register')->name('register');
+    Route::post('register', 'registerSave')->name('register.save');
+});
+
+// Route::get('register', 'AuthController@register')->name('register');
